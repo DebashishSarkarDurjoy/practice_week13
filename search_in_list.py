7d9fc30e669dd6544e1af7b724206181726ecf56
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-04-01 01:19:46 +1100

    delete implemented

--- search_in_list.py
+++ search_in_list.py
@@ -15,14 +15,23 @@ def insert_node(head: ListNode, data: int):
     temp.next = ListNode(data)
 
 def show_list(L: ListNode):
+    print()
     while L :
         print(f'{L.data} -> ', end="")
         L = L.next
+
+# delete the node past this one assuming it is not the tail node
+def delete_node(L: ListNode):
+    L.next = L.next.next
 
 if __name__ == "__main__":
     head = ListNode(0)
 
     for i in range(1,10):
         insert_node(head, i)
+
+    show_list(head)
+
+    delete_node(search_list(head, 5))
 
     show_list(head)
